cad63407f0f9f87fda11fa24b11716d64dcc9349
Mark Sutherland <mark@marksuth.dev>
2024-09-04 22:24:20 +0100

    marksuth.dev-32 RSS feed encoding

--- resources/views/feeds/posts.blade.php
+++ resources/views/feeds/posts.blade.php
@@ -6,7 +6,7 @@
             <author><![CDATA[Mark Sutherland]]></author>
             <link>{{ config('app.url') }}/posts/{{ $post->published_at->format('Y/m') }}/{{ $post->slug }}</link>
             <pubDate>{{ $post->published_at->tz(env('APP_TIMEZONE'))->toRfc2822String() }}</pubDate>
-            <description><![CDATA[{{ Str::markdown($post->content) }}]]></description>
+            <description>{{ Str::markdown($post->content) }}</description>
             <guid>{{ config('app.url') }}/posts/{{ $post->published_at->format('Y/m') }}/{{ $post->slug }}</guid>
         </item>
         @endforeach
